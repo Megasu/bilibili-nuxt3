d197575628446c028a188c5bd120d31b4958ee4c
苏杰豪 <Megasu@qq.com>
2023-07-26 20:02:49 +0800

    feat: SEO优化设置

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+// SEO 优化
+useSeoMeta({
+  title: '哔哩哔哩 (゜-゜)つロ 干杯~-bilibili官方',
+  description:
+    '哔哩哔哩（bilibili.com)是国内知名的视频弹幕网站，这里有及时的动漫新番，活跃的ACG氛围，有创意的Up主。大家可以在这里找到许多欢乐。',
+  // 搜索关键词
+  keywords: 'B站,bilibili,哔哩哔哩,哔哩哔哩动画,动漫,电影,在线动漫,高清电影',
+})
+</script>
+
 <template>
   <!-- Nuxt 基于文件的路由系统 -->
 
