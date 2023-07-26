844f4b2cd04909c87afaeaa9660ced8bde3b0572
苏杰豪 <Megasu@qq.com>
2023-07-26 21:15:34 +0800

    feat: 滚动触底加载分页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -5,6 +5,39 @@ const { data: channelList } = await useFetch('/api/channel')
 
 // 获取视频列表数据
 const { data: videoList } = await useFetch('/api/video')
+
+// 显示的列表
+const list = ref<any[]>([])
+// 加载状态
+const loading = ref(false)
+// 是否加载完成
+const finished = ref(false)
+
+// 页码
+let page = 1
+let pageSize = 20
+
+// 滚动触底触发
+const onLoad = () => {
+  // 表示正在加载
+  loading.value = false
+  // 根据当前页码提取数据
+  const data = videoList.value?.slice(
+    (page - 1) * pageSize,
+    page * pageSize,
+  ) as any[]
+  // 追加到用于渲染的数组中
+  list.value.push(...data)
+  // 页码累加
+  page++
+  // 加载结束
+  if (videoList.value?.length === list.value.length) {
+    finished.value = true
+  }
+}
+
+// 初始化加载 - 主动请求前 20 条数据，用于首屏渲染，方便 SEO 抓取到数据
+onLoad()
 </script>
 
 <template>
@@ -26,31 +59,38 @@ const { data: videoList } = await useFetch('/api/video')
     <van-tab v-for="item in channelList" :key="item.id" :title="item.name" />
   </van-tabs>
   <!-- 视频列表 -->
-  <div class="video-list">
-    <NuxtLink
-      class="v-card"
-      v-for="item in videoList"
-      :key="item.aid"
-      :to="`/video`"
-    >
-      <div class="card">
-        <div class="card-img">
-          <img class="pic" :src="item.pic" :alt="item.title" />
+  <van-list
+    v-model:loading="loading"
+    :finished="finished"
+    finished-text="去 bilibili App 看更多"
+    @load="onLoad"
+  >
+    <div class="video-list">
+      <NuxtLink
+        class="v-card"
+        v-for="item in list"
+        :key="item.aid"
+        :to="`/video`"
+      >
+        <div class="card">
+          <div class="card-img">
+            <img class="pic" :src="item.pic" :alt="item.title" />
+          </div>
+          <div class="count">
+            <span>
+              <i class="iconfont icon_shipin_bofangshu"></i>
+              {{ item.stat.view }}
+            </span>
+            <span>
+              <i class="iconfont icon_shipin_danmushu"></i>
+              {{ item.stat.danmaku }}
+            </span>
+          </div>
         </div>
-        <div class="count">
-          <span>
-            <i class="iconfont icon_shipin_bofangshu"></i>
-            {{ item.stat.view }}
-          </span>
-          <span>
-            <i class="iconfont icon_shipin_danmushu"></i>
-            {{ item.stat.danmaku }}
-          </span>
-        </div>
-      </div>
-      <p class="title">{{ item.title }}</p>
-    </NuxtLink>
-  </div>
+        <p class="title">{{ item.title }}</p>
+      </NuxtLink>
+    </div>
+  </van-list>
 </template>
 
 <style lang="scss">
