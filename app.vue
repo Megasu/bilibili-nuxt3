b6d509ea207eafb8986aa52178b0409e16edd5bd
苏杰豪 <Megasu@qq.com>
2023-07-26 22:25:49 +0800

    feat: 页面缓存和防盗链

--- app.vue
+++ app.vue
@@ -6,12 +6,15 @@ useSeoMeta({
     '哔哩哔哩（bilibili.com)是国内知名的视频弹幕网站，这里有及时的动漫新番，活跃的ACG氛围，有创意的Up主。大家可以在这里找到许多欢乐。',
   // 搜索关键词
   keywords: 'B站,bilibili,哔哩哔哩,哔哩哔哩动画,动漫,电影,在线动漫,高清电影',
+  // 绕开 B 站图片防盗链
+  referrer: 'no-referrer',
 })
 </script>
 
 <template>
   <!-- Nuxt 页面路由，相当于 RouterView -->
-  <NuxtPage />
+  <!-- 缓存 10 级页面，提升用户切换体验 -->
+  <NuxtPage :keepalive="{ max: 10 }" />
 </template>
 
 <style lang="scss">
